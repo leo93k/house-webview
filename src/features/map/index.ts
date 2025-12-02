23d12613939e97bb6978d37cbd03ad0eb1f5e02e
leo <leo@funble.kr>
2025-12-02 11:27:08 +0900

    feat: map feature 통합 export 추가
    - 모든 map feature 관련 export를 index.ts에 통합

---
+++ src/features/map/index.ts
@@ -0,0 +1,13 @@
+export { useKakaoMap } from "./hooks/useKakaoMap";
+export { useMapPostMessage } from "./hooks/useMapPostMessage";
+export { default as KakaoMapContainer } from "./components/KakaoMapContainer";
+export { default as PropertySidebar } from "./components/PropertySidebar";
+export type { PropertyItem } from "./types/map.types";
+export type {
+    PostMessage,
+    VisibleItemsUpdateMessage,
+    KakaoMapErrorMessage,
+    MessageTypeValue,
+} from "./types/mapPostMessage.types";
+export { MessageType } from "./types/mapPostMessage.types";
+
